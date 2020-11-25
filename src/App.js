a2fe4c96f951d440cba637025f22c7038216074a
Diego Ortiz <diegoortiz@Marias-MBP.attlocal.net>
2020-11-25 15:26:45 -0600

    test

--- src/App.js
+++ src/App.js
@@ -1,41 +1,73 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-import Header from './Header';
-import TodoList from './TodoList';
-import SubmitForm from './SubmitForm';
+import React from 'react';
 import './App.css';
 
+function Todo({ todo, index, removeTodo }) {
 
-class App extends Component {
-  state = { tasks: ['task 1', 'task 2', 'task 3'] };
-
-
-  handleDelete = (index) => {
-    const newArr = [...this.state.tasks];
-    newArr.splice(index, 1);
-    this.setState({ tasks: newArr });
-  }
-
-  handleSubmit = task => {
-    this.setState({ tasks: [...this.state.tasks, task] });
-  }
-
-
-
-  render() {
     return (
-      <div className="wrapper">
-        <Header numTodos={this.state.tasks.length} />
-        <TodoList tasks={this.state.tasks} onDelete={this.handleDelete} />
-        <SubmitForm onFormSubmit={this.handleSubmit} />
-      </div>
-    )
-  }
+        <div className="todo">
+            {todo.text}
+            <button onClick={() => removeTodo(index)}>X</button>
+        </div>
+    );
 };
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+function TodoForm({ addTodo }) {
+    const [value, setValue] = React.useState("");
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        if (!value) return;
+        addTodo(value);
+        setValue("");
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input
+                type="text"
+                className="input"
+                value={value}
+                onChange={e => setValue(e.target.value)}
+            />
+            <button>Submit</button>
+        </form>
+    );
+};
+
+function App() {
+    const [todos, setTodos] = React.useState([
+        { text: "do this" },
+        { text: "do this" },
+        { text: "do this" }
+    ]);
+
+    const addTodo = text => {
+        const newTodos = [...todos, { text }];
+        setTodos(newTodos)
+    }
+
+    const removeTodo = index => {
+        const newTodos = [...todos];
+        newTodos.splice(index, 1);
+        setTodos(newTodos)
+    }
+
+
+    return (
+        <div className="app">
+            <div className="todo-list">
+                {todos.map((todo, index) => (
+                    <Todo
+                        key={index}
+                        index={index}
+                        todo={todo}
+                        removeTodo={removeTodo}
+                    />
+                ))}
+                <TodoForm addTodo={addTodo} />
+            </div>
+        </div>
+    )
+};
 
 export default App;
